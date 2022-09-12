c3fd43a88aa70a3a559d14819b6314f4441a12b8
Artem Zlobenko <artemzlobenko@gmail.com>
2022-09-12 16:05:09 +0300

    Test

--- admin_commands.py
+++ admin_commands.py
@@ -9,8 +9,9 @@ from user import User
 from config import ADMIN_TG_ID
 
 
-async def stat(update: Update, context: ContextTypes.DEFAULT_TYPE): 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='stat_message',
-    )
+async def stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id == ADMIN_TG_ID:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='stat_message',
+        )
