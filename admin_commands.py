b661f23f2e6c7d5373f8e5ddb7b252afa1e38d62
Artem Zlobenko <artemzlobenko@gmail.com>
2022-09-12 16:22:48 +0300

    Fix checking different types

--- admin_commands.py
+++ admin_commands.py
@@ -11,7 +11,18 @@ from config import ADMIN_TG_ID
 
 async def stat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if str(update.effective_user.id) == ADMIN_TG_ID:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=update.effective_user.id + '     ' + ADMIN_TG_ID,
-        )
+        stat = await User.get_stat()
+        if stat:
+            stat_message = ''
+            for user in stat:
+                stat_message += ' | '.join(user) + '\n'
+                
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=stat_message,
+            )
+        else:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text='There are no users',
+            )
