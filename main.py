c6ed6590db483ae638230730d3d4568263734c23
Artem Zlobenko <artemzlobenko@gmail.com>
2022-09-15 23:01:38 +0300

    Fix csv handle

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import logging
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler
 from telegram.ext.filters import Document, Text
 
-from image import Image
 from bot_commands import start, images
 from admin_commands import stat, add_images
 from bot_commands import GET_IMAGES_TEXT
@@ -12,14 +11,14 @@ from config import BOT_TOKEN, CSV_PATH
 def main():
     logging.basicConfig(
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO
+        level=logging.DEBUG
     )
     
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     start_handler = CommandHandler('start', start)
     images_handler = MessageHandler(Text(GET_IMAGES_TEXT), images)
     admin_stat_handler = CommandHandler('stat', stat)
-    admin_add_images_handler = MessageHandler(Document.MimeType('application/csv'),
+    admin_add_images_handler = MessageHandler(Document.MimeType('text/csv'),
                                               add_images)
     application.add_handler(admin_stat_handler)
     application.add_handler(start_handler)
